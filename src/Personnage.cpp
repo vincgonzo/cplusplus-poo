55ba26efd140a0a97f6ca5b90b1a10588c406dc7
vincgonzo@hotmail.fr <vincgonzo@hotmail.fr>
2019-11-25 08:38:53 +0100

    overload constructor

--- src/Personnage.cpp
+++ src/Personnage.cpp
@@ -9,7 +9,10 @@ Personnage::Personnage()
     m_nomArme = "Epee rouillee";
     m_degatsArme = 5;
 }
+Personnage::Personnage(string nomArme, int degatsArme) : m_vie(100), m_mana(100),m_nomArme(nomArme), m_degatsArme(degatsArme)
+{
 
+}
 void Personnage::recevoirDegats(int nbDegats)
 {
     m_vie -= nbDegats;
--- src/Personnage.h
+++ src/Personnage.h
@@ -8,6 +8,7 @@ class Personnage
     public:
 
     Personnage();
+    Personnage(std::string nomArme, int degatsArme);
     void recevoirDegats(int nbDegats);
     void attaquer(Personnage &cible);
     void boirePotionDeVie(int quantitePotion);
